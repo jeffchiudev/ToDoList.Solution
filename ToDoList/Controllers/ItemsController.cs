508d32ea863cf1e011a0de1c835294c3b7c3119d
Jeff Chiu <jwchiu82@gmail.com>
2020-12-22 21:17:30 -0800

    add edit functions with EF core

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,34 +1,59 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-        [HttpGet("/categories/{categoryId}/items/new")]
-        public ActionResult New(int categoryId)
+        private readonly ToDoListContext _db;
+
+        public ItemsController(ToDoListContext db)
         {
-            Category category = Category.Find(categoryId);
-            return View(category);
+        _db = db;
         }
 
-        [HttpGet("/categories/{categoryId}/items/{itemId}")]
-        public ActionResult Show(int categoryId, int itemId)
+        public ActionResult Index()
         {
-            Item item = Item.Find(itemId);
-            Category category = Category.Find(categoryId);
-            Dictionary<string, object> model = new Dictionary<string, object>();
-            model.Add("item", item);
-            model.Add("category", category);
-            return View(model);
+        List<Item> model = _db.Items.ToList();
+        return View(model);
         }
 
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
+        public ActionResult Create()
         {
-            Item.ClearAll();
             return View();
         }
+        public ActionResult Details(int id)
+        {
+            Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        
     }
 }
